14a7f151be36d0217643afb395c8e40a4d6ad409
Julien Salomé <j.salome.pro@gmail.com>
2024-05-15 10:13:31 +0200

    test(data_types.js): add exercises

---
+++ data_types.js
@@ -0,0 +1,18 @@
+// https://javascript.info/types
+
+/* ------------------------------------------
+
+Travailler avec des variables
+
+Déclarez deux variables : admin and name.
+Assignez la valeur "John" à name.
+Copiez la valeur de name à admin.
+Afficher la valeur de admin en utilisant alert (devrait afficher “John”).
+
+*/
+
+// hello 1
+
+// name
+
+// hello Ilya
