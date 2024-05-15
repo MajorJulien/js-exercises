75e02bdefc177daa5850bf22bd3fda88e043d5c8
Julien Salomé <j.salome.pro@gmail.com>
2024-05-15 14:04:43 +0200

    feat(comparison.js): add exercises

---
+++ comparison.js
@@ -0,0 +1,25 @@
+https://fr.javascript.info/comparison
+
+/* ------------------------------------------
+
+Comparaisons
+
+Quel sera le résultat pour les expressions suivantes :
+
+5 > 4
+"apple" > "pineapple"
+"2" > "12"
+undefined == null
+undefined === null
+null == "\n0\n"
+null === +"\n0\n"
+
+*/
+
+// 1: true
+// 2: false
+// 3: true
+// 4: true
+// 5: false
+// 6: false
+// 7: false
