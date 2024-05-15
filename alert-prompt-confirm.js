e864d4fbdeb88c7fb131a0ded57712a683832c6f
Julien Salomé <j.salome.pro@gmail.com>
2024-05-15 10:34:46 +0200

    feat(alert-prompt-confirm.js): add exercise

---
+++ alert-prompt-confirm.js
@@ -0,0 +1,14 @@
+// https://fr.javascript.info/alert-prompt-confirm
+
+/* ------------------------------------------
+
+Une simple page
+
+Créez une page Web qui demande un nom et l’affiche
+
+*/
+
+// Script ci-dessous contenu entre deux balises <script> à insérer entre les balises <body> du code HTML
+
+let name = prompt("What is your name ?");
+console.log(name);
