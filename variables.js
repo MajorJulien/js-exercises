1e7777780ce0de1af2f571d73a0b9bb6bc17741f
Julien Salomé <j.salome.pro@gmail.com>
2024-05-15 10:06:57 +0200

    test(variables.js): add exercises

---
+++ variables.js
@@ -0,0 +1,31 @@
+// https://fr.javascript.info/variables
+
+/* ------------------------------------------
+
+Travailler avec des variables
+
+Déclarez deux variables : admin and name.
+Assignez la valeur "John" à name.
+Copiez la valeur de name à admin.
+Afficher la valeur de admin en utilisant alert (devrait afficher “John”).
+
+*/
+
+let admin, name;
+
+name = "John";
+admin = name;
+
+console.log(admin);
+
+/* ------------------------------------------
+
+Assigner le bon nom
+
+Créez la variable avec le nom de notre planète. Comment nommeriez-vous une telle variable ?
+Créez la variable pour stocker le nom du visiteur actuel. Comment nommeriez-vous cette variable ?
+
+*/
+
+let NotrePlanete = "Terre";
+let CurrentUserName = "John";
