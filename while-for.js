3abf8af91fc5d821f1fe767eee17e1e3ce0eb830
Julien Salomé <j.salome.pro@gmail.com>
2024-05-17 09:05:37 +0200

    feat(while-for.js): add exercise

---
+++ while-for.js
@@ -0,0 +1,36 @@
+/* ------------------------------------------
+
+Dernière valeur de boucle
+importance: 3
+Quelle est la dernière valeur affichée par ce code ? Pourquoi ?
+
+let i = 3;
+
+while (i) {
+  alert( i-- );
+}
+
+*/
+
+// REPONSE : 1
+
+/* ------------------------------------------
+
+Quelles valeurs affiche la boucle while ?
+importance: 4
+A votre avis, quelles sont les valeurs affichées pour chaque boucle ? Notez-les puis comparer avec la réponse.
+
+Les deux boucles affichent-elles les mêmes valeurs dans l’alert ou pas ?
+
+Le préfixe sous forme ++i :
+
+let i = 0;
+while (++i < 5) alert( i );
+Le postfixe sous forme i++ :
+
+let i = 0;
+while (i++ < 5) alert( i );
+
+*/
+
+// 
